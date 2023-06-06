49cf5833e6efe65403c46cd2f7baebf52ae22714
ChiPre01 <ogaizi70@gmail.com>
2023-06-06 17:14:08 +0100

    second

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -20,7 +20,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
   final TextEditingController _desciptionController = TextEditingController();
   bool _isLoading = false;
 
-  void getVideo(
+  void postVideo(
     String uid,
     String username,
     String profImage,
@@ -138,7 +138,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
               centerTitle: false,
               actions: [
                 TextButton(
-                  onPressed: () => getVideo(
+                  onPressed: () => postVideo(
                     user.uid,
                     user.username,
                     user.photoUrl,
